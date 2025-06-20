899255722f3460fe0c2edb693056c2f8989d009a
EveGlowLuna <yy15502497613@outlook.com>
2025-06-20 04:37:36 +0000

    Deploying to gh-pages from @ Shasnow/Shasnow.github.io@e3b2f5417bfdc5abac732f054184b9c0f371f8d8 🚀

--- SRA/asset/js/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-toggle').addEventListener('click', function () {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.toggle('active');
-});
