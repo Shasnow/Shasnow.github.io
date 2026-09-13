89e3e8e5d9a793766d6403a7b8e38269d704990f
Shasnow <yukikage@qq.com>
2026-09-13 21:53:11 +0800

    feat: 新增蔚蓝档案三个服的游戏数据
    perf: 更新异环活动封面图为最新资源
    
    docs: 添加无配置类扩展的开发教程文档
    
    chore: 移除旧的2.13版本更新公告

--- src/data/games.ts
+++ src/data/games.ts
@@ -4,4 +4,7 @@ export const games = [
   { id: "zzz", name: "绝区零" },
   { id: "ww", name: "鸣潮" },
   { id: "nte", name: "异环" },
+  { id: "ba-cn", name: "蔚蓝档案-国服" },
+  { id: "ba-global", name: "蔚蓝档案-国际服" },
+  { id: "ba-jp", name: "蔚蓝档案-日服" },
 ];
